ba05847bd08df5e9933767321f2a78f9f210aa2b
Simran Narang <narang.simran30@gmail.com>
2018-06-05 09:58:42 +0530

    Adding tooltip function in js

--- includes/js/script.js
+++ includes/js/script.js
@@ -8,15 +8,7 @@ Updated: June 2018
 */
 
 $(function(){
-	$('#alertMe').click(function(e){
-		e.preventDefault();
-		$('#successAlert').slideDown();
-	});
 
-	$('a.pop').click(function(e){
-		e.preventDefault();
-	});
-	$('a.pop').popover();
 
 	$('[rel="tooltip"]').tooltip();
 });
